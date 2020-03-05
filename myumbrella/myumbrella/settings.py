0d4dc5d9e74e3c2ee08981e943a7e185405ae924
= <2041778+vinicius191@users.noreply.github.com>
2020-03-05 21:07:22 +1100

    .

--- myumbrella/myumbrella/settings.py
+++ myumbrella/myumbrella/settings.py
@@ -177,7 +177,7 @@ STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-django_heroku.settings(locals(), staticfiles=False))
+django_heroku.settings(locals(), staticfiles=False)
 
 # This is new for use on Heroku (PostGresSQL)
 del DATABASES['default']['OPTIONS']['sslmode']
