772e64584cca45359e326548204c9e576fef129c
= <2041778+vinicius191@users.noreply.github.com>
2020-03-05 21:35:32 +1100

    .

--- myumbrella/myumbrella/settings.py
+++ myumbrella/myumbrella/settings.py
@@ -169,6 +169,8 @@ AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_LOCATION = 'static'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
